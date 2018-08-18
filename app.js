84395ec4033510e0111f1a3fa76022a4066ca2c0
Richard Garner <richardgarner@mac.com>
2018-08-17 19:59:13 -0500

    8 P.M. Lab complete

---
+++ app.js
@@ -0,0 +1,44 @@
+var friends = ['Chris', 'Warryn', 'Woody', 'Jun', 'Senpai']
+
+for (let f = 0; f < friends.length; f++) {
+    console.log(friends[f] + ':')
+    for (let n = 99; n < 100; n--) {
+        if (n === 1) {
+            console.log(n + " lines of code in the file, " + n + " lines of code; " + [friends[f]] + " strikes one out, clears it all out, no more lines of code in the file!")
+            break
+        } else {
+            console.log(n + " lines of code in the file, " + n + " lines of code; " + [friends[f]] + " strikes one out, clears it all out, " + [n - 1] + " lines of code in the file!");
+        }
+    }
+}
+// Everything belo here are previous attempts as well as test i ran to see if things were even possible in the first place
+//
+//
+//for (let key in friends) {
+//    console.log(friends[key])
+//    for (let index = 99; index > 100; index--) {
+//        if (index === 1) {
+//            console.log(index + " lines of code in the file, " + index + " lines of code; " + [friends[key]] + " strikes one out, clears it all out, no more lines of code in the file!")
+//            break
+//        } else {
+//            console.log(friends + ':')
+//            console.log(index + " lines of code in the file, " + index + " lines of code; " + [friends[key]] + " strikes one out, clears it all out, " + [index - 1] + "lines of code in the file!");
+//        }
+//    }
+//}
+
+//friends.forEach(element => {
+//    for (let index = 99; index > 100; index--) {
+//        if (index === 1) {
+//            console.log(index + " lines of code in the file, " + index + " lines of code; " + [friends[key]] + " strikes one out, clears it all out, no more lines of code in the file!")
+//            break
+//        } else {
+//            console.log(friends + ':')
+//            console.log(index + " lines of code in the file, " + index + " lines of code; " + [friends[key]] + " strikes one out, clears it all out, " + [index - 1] + "lines of code in the file!");
+//        }
+//    }
+//});
+//
+//var test = (10)
+//
+//console.log('hello world ' + [test - 1])
