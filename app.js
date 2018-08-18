48cc49f24580ffb967b347316c788d3343ee0bfd
Richard Garner <richardgarner@mac.com>
2018-08-17 20:29:26 -0500

    8:30 P.M. fixed another typo

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ var friends = ['Chris', 'Warryn', 'Woody', 'Jun', 'Anthony']
 for (let f = 0; f < friends.length; f++) {
     console.log(friends[f] + ':')
     for (let n = 99; n < 100; n--) {
-        if (n === 1) {
-            console.log(n + " lines of code in the file, " + n + " lines of code; " + [friends[f]] + " strikes one out, clears it all out, no more lines of code in the file!")
-            break
+        if (n === 2) {
+            console.log(n + " lines of code in the file, " + n + " lines of code; " + [friends[f]] + " strikes one out, clears it all out, " + [n - 1] + " line of code in the file!");
         } else {
-            console.log(n + " lines of code in the file, " + n + " lines of code; " + [friends[f]] + " strikes one out, clears it all out, " + [n - 1] + " lines of code in the file!");
+            if (n === 1) {
+                console.log(n + " line of code in the file, " + n + " line of code; " + [friends[f]] + " strikes one out, clears it all out, no more lines of code in the file!")
+                break
+            } else {
+                console.log(n + " lines of code in the file, " + n + " lines of code; " + [friends[f]] + " strikes one out, clears it all out, " + [n - 1] + " lines of code in the file!");
+            }
         }
+        
     }
 }
 // Everything belo here are previous attempts as well as test i ran to see if things were even possible in the first place
